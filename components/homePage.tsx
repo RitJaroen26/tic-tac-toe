660b1a177fb813a9c38c22211b374d7e6e394aee
RitJaroen26 <ponpawarit21@gmail.com>
2025-10-08 00:58:48 +0700

    add login and regisster

--- components/homePage.tsx
+++ components/homePage.tsx
@@ -8,16 +8,16 @@ export default function HomePage() {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen gap-8">
             <h1 className="text-3xl font-bold">RafficToe</h1>
-
+            
             <div className="flex flex-col gap-4">
                 <button
-                    className="px-6 py-3 bg-blue-500 text-white rounded hover:bg-blue-600"
+                    className="cursor-pointer px-6 py-3 bg-blue-500 text-white rounded hover:bg-blue-600"
                     onClick={() => router.push("/single-player")}
                 >   
                     Singleplayer
                 </button>
                 <button
-                    className="px-6 py-3 bg-blue-500 text-white rounded hover:bg-blue-600"
+                    className="cursor-pointer px-6 py-3 bg-blue-500 text-white rounded hover:bg-blue-600"
                     onClick={() => router.push("/multi-player")}
                 >
                     Multiplayer
