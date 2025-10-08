9ed7dd6d4ec9e3100f79b97273d102aeabcfdb24
RitJaroen26 <ponpawarit21@gmail.com>
2025-10-08 13:56:19 +0700

    edit ai

--- lib/ai.ts
+++ lib/ai.ts
@@ -1,28 +1,45 @@
 import { checkWinner } from "@/utils/checkWinner";
 
 export function GetAIMove(board: string[]): number {
-    const empty: number[] = board
-        .map((v, i) => (v === "" ? i : -1))
-        .filter((i): i is number => i !== -1);
+  // const empty: number[] = board
+  //     .map((v, i) => (v === "" ? i : -1))
+  //     .filter((i): i is number => i !== -1);
 
-    if (empty.length === 0) return -1;
+  // if (empty.length === 0) return -1;
 
-    for (const i of empty) {
-        const copy = [...board];
-        copy[i] = "O";
-        if (checkWinner(copy).winner === "O") return i;
-    }
+  // for (const i of empty) {
+  //     const copy = [...board];
+  //     copy[i] = "O";
+  //     if (checkWinner(copy).winner === "O") return i;
+  // }
 
-    for (const i of empty) {
-        const copy = [...board];
-        copy[i] = "X";
-        if (checkWinner(copy).winner === "X") return i;
-    }
+  // for (const i of empty) {
+  //     const copy = [...board];
+  //     copy[i] = "X";
+  //     if (checkWinner(copy).winner === "X") return i;
+  // }
 
-    const preferredMoves = [4, 0, 2, 6, 8, 1, 3, 5, 7];
-    for (const i of preferredMoves) {
-        if (empty.includes(i)) return i;
-    }
+  // const preferredMoves = [4, 0, 2, 6, 8, 1, 3, 5, 7];
+  // for (const i of preferredMoves) {
+  //     if (empty.includes(i)) return i;
+  // }
 
-    return empty[Math.floor(Math.random() * empty.length)];
+  // return empty[Math.floor(Math.random() * empty.length)];
+
+  const empty: number[] = board
+    .map((v, i) => (v === "" ? i : -1))
+    .filter((i): i is number => i !== -1);
+  if (empty.length === 0) return -1;
+  for (const i of empty) {
+    const copy = [...board];
+    copy[i] = "O";
+    if (checkWinner(copy).winner === "O") return i;
+  }
+  for (const i of empty) {
+    const copy = [...board];
+    copy[i] = "X";
+    if (checkWinner(copy).winner === "X") return i;
+  }
+  if (empty.includes(4)) return 4;
+  return empty[Math.floor(Math.random() * empty.length)];
 }
